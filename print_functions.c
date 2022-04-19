d085d929bac5687b27aff7f24e91e9844f85b562
root <oumich37@gmail.com>
2022-04-19 11:31:55 -0700

    betty style

--- print_functions.c
+++ print_functions.c
@@ -2,22 +2,22 @@ include "main.h"
 #include <unistd.h>
 #include <stdio.h>
 /**
- * print_char - writes the character c to stdout
- * @arg: argument
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* print_char - writes the character c to stdout
+* @arg: argument
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int print_char(va_list arg)
 {
-	return (_putchar(va_arg(arg, int)));
+return (_putchar(va_arg(arg, int)));
 }
 
 /**
- * print_int - prints an integer.
- * @arg: argument
- * Return: 0
- */
+* print_int - prints an integer.
+* @arg: argument
+* Return: 0
+*/
 
 int print_int(va_list arg)
 {
@@ -27,9 +27,9 @@ int n = va_arg(arg, int);
 
 if (n < 0)
 {
-	_putchar('-');
-	charPrinted++;
-	n *= -1;
+_putchar('-');
+charPrinted++;
+n *= -1;
 }
 
 for (i = 0; n / divisor > 9; i++, divisor *= 10)
@@ -37,8 +37,8 @@ for (i = 0; n / divisor > 9; i++, divisor *= 10)
 
 for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
+resp = n / divisor;
+_putchar('0' + resp);
 }
 return (charPrinted);
 }
@@ -46,10 +46,10 @@ return (charPrinted);
 
 
 /**
- * print_STR - prints a string with a `S` (upper case) specificer
- * @arg: argument
- * Return: number of character printed
- */
+* print_STR - prints a string with a `S` (upper case) specificer
+* @arg: argument
+* Return: number of character printed
+*/
 
 int print_STR(va_list arg)
 {
@@ -57,33 +57,33 @@ int i;
 char *str = va_arg(arg, char*);
 
 if (str == NULL)
-	str = "(null)";
+str = "(null)";
 else if (*str == '\0')
-	return (-1);
+return (-1);
 
 for (i = 0; str[i]; i++)
 {
-	if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
-	{
-		_putchar('\\');
-		_putchar('x');
-		if (i < 16)
-			_putchar('0');
+if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
+{
+_putchar('\\');
+_putchar('x');
+if (i < 16)
+_putchar('0');
 
-		print_unsignedIntToHex(str[i], 'A');
-	}
-	else
-		_putchar(str[i]);
+print_unsignedIntToHex(str[i], 'A');
+}
+else
+_putchar(str[i]);
 }
 
 return (i);
 }
 
 /**
- * print_str - prints a string with a `s` (lower case) specifier
- * @arg: argument
- * Return: number of character printed
- */
+* print_str - prints a string with a `s` (lower case) specifier
+* @arg: argument
+* Return: number of character printed
+*/
 
 int print_str(va_list arg)
 {
@@ -91,21 +91,21 @@ int i;
 char *str = va_arg(arg, char*);
 
 if (str == NULL)
-	str = "(null)";
+str = "(null)";
 else if (*str == '\0')
-	return (-1);
+return (-1);
 
 for (i = 0; str[i]; i++)
-	_putchar(str[i]);
+_putchar(str[i]);
 
 return (i);
 }
 
 /**
- * print_unsigned - prints an unsigned int.
- * @arg: argument
- * Return: 0
- */
+* print_unsigned - prints an unsigned int.
+* @arg: argument
+* Return: 0
+*/
 
 int print_unsigned(va_list arg)
 {
@@ -117,8 +117,8 @@ for (i = 0; n / divisor > 9; i++, divisor *= 10)
 
 for (; divisor >= 1; n %= divisor, divisor /= 10)
 {
-	resp = n / divisor;
-	_putchar('0' + resp);
+resp = n / divisor;
+_putchar('0' + resp);
 }
 return (i + 1);
 }
