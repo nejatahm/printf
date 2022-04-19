2ee2412f8b8c0829d91b03c2857dc95f7df1d267
nejatahm <nejah2005@gmail.com>
2022-04-19 10:07:27 -0700

    program to write character for standard output

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,30 @@
 #include <unistd.h>
+
 /**
-*_putchar - write to stdout.
-*@c: char to be written.
-*
-*Return: number of chars written.
-*/
-int _putchar(char c)
+ * _putchar - print char with stdout
+ * @ch: char to print
+ * Return: Output.
+ */
+int _putchar(char ch)
 {
-return (write(1, &c, 1));
+	static int contador;
+	static char buffer[1024];
+
+	if (ch == -1)
+	{
+		contador = 0;
+		return (0);
+	}
+	if (ch == -2 || contador == 1024)
+	{
+		write(1, buffer, contador);
+		contador = 0;
+	}
+	if (ch != -1 && ch != -2)
+	{
+		buffer[contador] = ch;
+		contador++;
+		return (1);
+	}
+	return (0);
 }
